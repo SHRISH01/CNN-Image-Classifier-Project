153c8018f1ef886ac9a849ca29f2d6e6d5e5b9ed
Shrish Kamboj <shrishkamboz@gmail.com>
2024-12-11 22:17:41 +0530

    base model setup

---
+++ src/CNNClassifier/components/stage_02_prepare_base_model.py
@@ -0,0 +1,63 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+from pathlib import Path
+from src.CNNClassifier.entity import PrepareBaseModelConfig
+import tensorflow as tf
+
+
+class PrepareBaseModel:
+    def __init__(self,config,PrepareBseModelConfig):
+        self.config = config
+
+    def get_base_model(self,):
+        self.model = tf.Keras.applications.vgg16.VGG16(
+            input_shape=self.config.params_image_size,
+            weights=self.config.params_weight,
+            include_top=self.config.params_include_top
+        )
+        self.save_model(path=self.config.base_model_path,model=self.model)
+
+    @staticmethod
+    def prepare_full_model(model,classes,freeze_all,freeze_till,learning_rate):
+        if freeze_all:
+            model.trainable=False
+        elif (freeze_till is None) or (freeze_till>0):
+            for layer in model.layers[:-freeze_till]:
+                layer.trainable=False
+
+        flatten_in = tf.keras.layers.Flatten()(model.output)
+        prediction = tf.keras.layers.Dense(
+            units=Classes,
+            activation='softmax'
+        )(flatten_in)
+
+        full_model = tf.keras.models.Mode(
+            inputs=model.input,
+            outputs=prediction
+        )
+
+        full_model.compile(
+            optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+            loss=tf.keras.losses.CategoricalCrossentropy(),
+            metrics=["accuracy"]
+        )
+
+        full_model.summary()
+        return full_model
+
+    def update_base_model(self):
+        self.full_model = self.prepare_full_model(
+            model=self.model,
+            classes=self.config.params_classes,
+            freeze_all=True,
+            freeze_till=None,
+            learning_rate=self.config.params_learning_rate
+        )
+
+        self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
+
+    @staticmethod
+    def save_model(path: Path, model: tf.keras.Model):
+        model.save(path)
+        
---
+++ src/CNNClassifier/pipeline/stage_02_base_model.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+from src.CNNClassifier.config import ConfigurationManager
+from src.CNNClassifier.components import PrepareBasemModel
+from src.CNNClassifier import logger
+
+try:
+    config = ConfigurationManager()
+    prepare_base_model_config = config.get_prepare_base_model_config()
+    prepare_base_model = PrepareBasemModel(config=prepare_base_model_config)
+    prepare_base_model.get_base_model()
+    prepare_base_model.update_base_model()
+except Exception as e:
+    raise e
+
